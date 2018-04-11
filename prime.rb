bd8b278ad56ce32a611351cd72dd2de6d3459b7d
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-11 01:46:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime?(number)
   if
-    number < 2 && (2..number-1).to_a.any?{|i| number % i == 0} #=false
+    number < 2 || (2..number-1).to_a.any?{|i| number % i == 0} #=false
     return false
   else
     return true
   end
-
-
 end
 
 # number has to be 2 or greater
